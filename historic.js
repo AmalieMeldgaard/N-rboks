831758c3420c709ead374f5965a99b8ca396ce6d
AmalieMeldgaard <70745423+AmalieMeldgaard@users.noreply.github.com>
2021-05-27 10:34:08 +0200

    javascript historic

--- historic.js
+++ historic.js
@@ -1,7 +1,11 @@
-function openNav() {
-  document.getElementById("mySidenav").style.width = "100px";
+function myFunction() {
+  if (direction.classList.contains("displayNone")) {
+    direction.classList.remove("displayNone");
+  } else {
+    direction.classList.add("displayNone");
+  }
 }
 
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
+const btn = document.querySelector(".historic__img");
+const direction = document.querySelector(".package__infoJs");
+btn.addEventListener("click", myFunction);
