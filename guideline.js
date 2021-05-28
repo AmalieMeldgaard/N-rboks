53355f8c75b7e8ec05c1f47cc5ec3c344daa39d6
AmalieMeldgaard <70745423+AmalieMeldgaard@users.noreply.github.com>
2021-05-28 11:38:22 +0200

    guideline

--- guideline.js
+++ guideline.js
@@ -1,27 +1,38 @@
 const guideline = document.querySelector(".input__guide");
 const content = document.querySelectorAll(".guidebox");
+const content1 = document.querySelector(".content1");
+const content2 = document.querySelector(".content2");
+const content3 = document.querySelector(".content3");
+const content4 = document.querySelector(".content4");
 
 guideline.addEventListener("change", () => {
-  //console.log(guideline.value);
   let value = document.querySelector(".input__guide").value;
 
   let activeContent = "content" + value;
 
-  console.log(activeContent);
+  console.log(value);
+  console.log(content);
 
-  content.forEach((i) => {
-    i.classlist.add("hidden");
-  });
-
-  /* if (guideline.value === 1) {
-    content.forEach(i, () => {
-      i.classlist.add("hidden");
+  if (value == 1) {
+    content.forEach((i) => {
+      i.classList.add("hidden");
     });
     content1.classList.remove("hidden");
-  } else if (guideline.value === 2) {
-    content.forEach(i, () => {
-      i.classlist.add("hidden");
+  } else if (value == 2) {
+    content.forEach((i) => {
+      i.classList.add("hidden");
     });
     content2.classList.remove("hidden");
-  } */
+    console.log("removed2");
+  } else if (value == 3) {
+    content.forEach((i) => {
+      i.classList.add("hidden");
+    });
+    content3.classList.remove("hidden");
+  } else if (value == 4) {
+    content.forEach((i) => {
+      i.classList.add("hidden");
+    });
+    content4.classList.remove("hidden");
+  }
 });
