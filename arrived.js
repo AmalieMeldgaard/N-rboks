2218fefce16ab38684c1c33be8353a5716d86d4d
miebknudsen <70746334+miebknudsen@users.noreply.github.com>
2021-06-02 13:55:59 +0200

    Javascript to arrived and lågelukkes

--- arrived.js
+++ arrived.js
@@ -6,6 +6,12 @@ function play() {
   console.log("play was called");
   var audio = new Audio("mp3/notification.mp3");
   audio.play();
+
+  setTimeout(function() {window.location.href = "lågelukkes.html";}, 1500);
+  
+
+
+
 }
 
 // eventlistener for when clicking the <body>
--- lågelukkes.js
+++ lågelukkes.js
@@ -1,2 +1,5 @@
 // eventlistener for when clicking the <body>
 //document.querySelector("body").addEventListener("click", play);
+
+
+    setTimeout(function() {window.location.href = "home.html";}, 3300);
